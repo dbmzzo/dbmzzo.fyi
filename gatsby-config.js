c70baea655a286bad14d9860c24e59eee6f831e9
Daniel Matarazzo <daniel@able-archer.net>
2023-12-09 00:39:55 -0800

    update above-the-fold color

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,8 +41,8 @@ module.exports = {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#F2EFED',
+        theme_color: '#F2EFED',
         display: 'minimal-ui',
         icon: 'src/images/fav-icon.png', // This path is relative to the root of the site.
       },
